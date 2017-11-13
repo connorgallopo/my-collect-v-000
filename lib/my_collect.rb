2a1c3a08a466b99ffb6cfa2ce2d564b53f617614
Connor Gallopo <connor.gallopo@me.com>
2017-11-13 23:51:01 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   count = 0
   new_array = []
   while array.length > count
-    yield (array[count])
+    # yield (array[count])
     new_array << yield(array[count])
     count += 1
   end
