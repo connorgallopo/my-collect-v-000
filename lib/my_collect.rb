1c5ea581062158eb38616826b74d1701815d3a28
Connor Gallopo <connor.gallopo@me.com>
2017-11-13 23:44:10 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(array)
+  count = 0
+  # first_name = []
+  while array.length > count
+    yield array[count]
+    # array.first << first_name
+    count += 1
+  end 
+  first_name
+end
+    
 
